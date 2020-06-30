6069c16e86534046c10de68856ddbf8e3b1b7958
Ashwin <bsashwin9@gmail.com>
2020-06-30 19:48:19 +0530

    Add heading, sub-headings and install dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Col, Container, Jumbotron, Row} from 'react-bootstrap';
 
 class App extends React.Component {
     render() {
         return (
-            <div>Hello, World!</div>
-        );
+            <div>
+                <Jumbotron variant="light">
+                    <Container className="text-center">
+                        <h1>Markdown Previewer</h1>
+                    </Container>
+                </Jumbotron>
+                <Row className='m-0'>
+                    <Col md='6' className='text-center'>
+                        <p className="lead">Markdown Input</p>
+                    </Col>
+                    <Col md='6' className='text-center'>
+                        <p className="lead">Preview</p>
+                    </Col>
+                </Row>
+            </div>
+        )
     }
 }
 
